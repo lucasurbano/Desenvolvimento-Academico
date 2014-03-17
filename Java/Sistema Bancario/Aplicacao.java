1fc4b272b5c210eef2e9275437edc04c958dab6f
Lucas Urbano <lucasurbano@hotmail.com.br>
2014-03-16 22:01:43 -0300

    Inclusao de caixa de dialogo(Codigo fonte)
    Codigo Fonte

--- Java/Sistema Bancario/Aplicacao.java
+++ Java/Sistema Bancario/Aplicacao.java
@@ -7,56 +7,64 @@ public class Aplicacao {
 	public static void main (String []args) {
 		
 		// cria um cliente
+		// Cliente 1 
 		Cliente cliente1 = new Cliente();
-		cliente1.nome = "Cleyton";
-		cliente1.idade = 29;
-		cliente1.cpf = "000.000.000-11";
-		
+		cliente1.nome = JOptionPane.showInputDialog ("Informe o nome do Cliente 1 : ");
+		cliente1.idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a idade do Cliente : "+cliente1.nome));
+		cliente1.cpf = JOptionPane.showInputDialog ("Informe o CPF do Cliente : "+cliente1.nome);
+
 		// cria um cliente
+		// Cliente 2
 		Cliente cliente2 = new Cliente();
-		cliente2.nome = "Joselardisson";
-		cliente2.idade = 16;
-		cliente2.cpf = "000.000.001-21";
+		cliente2.nome = JOptionPane.showInputDialog ("Informe o nome do Cliente 2 : ");
+		cliente2.idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a idade do Cliente : "+cliente2.nome));
+		cliente2.cpf = JOptionPane.showInputDialog ("Informe o CPF do Cliente : "+cliente2.nome);
 		
 		// criar uma conta corrente
+		// Cliente 1 
 		ContaCorrente conta1 = new ContaCorrente();
-		conta1.agencia = 12;
-		conta1.nConta = 89;
-		conta1.saldo = 20.98;
+		conta1.agencia = Integer.parseInt (JOptionPane.showInputDialog(null,"Informe a Agencia do cliente: "+cliente1.nome));
+		conta1.nConta = Integer.parseInt (JOptionPane.showInputDialog(null,"Informe o numero da conta corrente do "+cliente1.nome));
+		conta1.saldo =  Double.parseDouble (JOptionPane.showInputDialog(null,"Informe o saldo da conta corrente do "+cliente1.nome));
 		conta1.cliente = cliente1;
 		
 		// criar uma conta corrente
+		// Cliente 2 
 		ContaCorrente conta2 = new ContaCorrente();
-		conta2.agencia = 12;
-		conta2.nConta = 45;
-		conta2.saldo = 120.68;
+		conta2.agencia = Integer.parseInt (JOptionPane.showInputDialog(null,"Informe a Agencia do cliente: "+cliente2.nome));
+		conta2.nConta = Integer.parseInt (JOptionPane.showInputDialog(null,"Informe o numero da conta corrente do "+cliente2.nome));
+		conta2.saldo =  Double.parseDouble (JOptionPane.showInputDialog(null,"Informe o saldo da conta corrente do "+cliente2.nome));
 		conta2.cliente = cliente2;
 		
-		// realiza um credito de 10
-		conta1.creditar(10);
+		// realiza um credito com valor informado pelo operador
+		conta1.creditar(Double.parseDouble (JOptionPane.showInputDialog(null,"Informe o valor de deposito do cliente "+cliente1.nome)));
+		conta2.creditar(Double.parseDouble (JOptionPane.showInputDialog(null,"Informe o valor de deposito do cliente "+cliente2.nome)));
 		
-		//realiza um debito de 100
-		boolean saque = conta1.sacar(1);
+		//realiza um debito de  com o valor informado pelo operador
+
+		//Cliente 1	
+		boolean saque = conta1.sacar(Double.parseDouble (JOptionPane.showInputDialog(null,"Informe o valor de saque do cliente "+cliente1.nome)));
 		if (saque) {
 			JOptionPane.showMessageDialog(null, "Saque realizado com sucesso");			
 		} else {
 			JOptionPane.showMessageDialog(null, "O Saldo da conta é insuficiente");
 		}
+
+		//Cliente 2	
+		boolean saque = conta2.sacar(Double.parseDouble (JOptionPane.showInputDialog(null,"Informe o valor de saque do cliente "+cliente2.nome)));
+		if (saque) {
+			JOptionPane.showMessageDialog(null, "Saque realizado com sucesso");			
+		} else {
+			JOptionPane.showMessageDialog(null, "O Saldo da conta é insuficiente");
+		}
+
+		
 		
 		// pega o saldo atual da conta
-		double saldo = conta1.saldoAtual();
+		//saldo = conta1.saldoAtual();
+		//saldo = conta2.saldoAtual();
 		
-		////
-		
-		// realiza um credito de 1000
-		conta2.creditar(56.98);
-		
-		//realiza um debito de 100
-		conta2.sacar(1200.87);
-		
-		// pega o saldo atual da conta
-		saldo = conta2.saldoAtual();
-		
+		////	
 		
 	}
 }
