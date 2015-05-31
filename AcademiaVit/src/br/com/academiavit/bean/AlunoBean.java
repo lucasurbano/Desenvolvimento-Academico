9a95a83cdaed61251e3c68ec98ab81ee4eb85da0
Lucas Urbano <lucasurbano@hotmail.com.br>
2015-05-31 19:28:01 -0300

    Alterações de tarcisio Pedro

---
+++ AcademiaVit/src/br/com/academiavit/bean/AlunoBean.java
@@ -0,0 +1,75 @@
+package br.com.academiavit.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.academiavit.dao.AlunoDAO;
+import br.com.academiavit.domain.Aluno;
+import br.com.academiavit.util.FacesUtil;
+
+@ManagedBean
+@ViewScoped
+public class AlunoBean {
+	private Aluno alunoCadastro;
+	private List<Aluno> listaAlunos;
+	private List<Aluno> listaAlunosFiltrados;
+
+	public Aluno getAlunoCadastro() {
+		if (alunoCadastro == null) {
+			alunoCadastro = new Aluno();
+		}
+		return alunoCadastro;
+	}
+
+	public void setAlunoCadastro(Aluno alunoCadastro) {
+		this.alunoCadastro = alunoCadastro;
+	}
+	
+	public List<Aluno> getListaAlunos(){
+		return listaAlunos;
+	}
+	
+	public void setListaAlunos(List<Aluno> listaAlunos){
+		this.listaAlunos = listaAlunos;
+	}
+	
+	public List<Aluno> getListaAlunosFiltrados(){
+		return listaAlunosFiltrados;
+	}
+	
+	public void setListaAlunosFiltrados(List<Aluno> listaAlunosFiltrados){
+		this.listaAlunosFiltrados = listaAlunosFiltrados;
+	}
+	
+	public void novo(){
+		alunoCadastro = new Aluno();
+	}
+
+	public void salvar() {
+		try {
+			AlunoDAO alunoDAO = new AlunoDAO();
+			alunoDAO.salvar(alunoCadastro);
+			
+			alunoCadastro = new Aluno();
+
+			FacesUtil.adicionarMsgInfo("Aluno Salvo com sucesso");
+		} catch (RuntimeException ex) {
+			ex.printStackTrace();
+			FacesUtil.adicionarMsgErro("Erro ao tentar incluir o aluno: "
+					+ ex.getMessage());
+		}
+	}
+	
+public void carregar(){
+		try{
+			AlunoDAO alunoDAO = new AlunoDAO();
+			listaAlunos = alunoDAO.listar();
+		}catch (RuntimeException ex) {
+			ex.printStackTrace();
+			FacesUtil.adicionarMsgErro("Erro ao tentar listar os alunos: "
+					+ ex.getMessage());
+	}
+	}
+}
